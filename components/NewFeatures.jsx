3bb508f2107ae46e55dbe33979aa377e5cdefd88
dirasanjayawardana <dirawardana.dw@gmail.com>
2023-05-09 20:41:42 +0700

    update whats new

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,10 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from "../styles";
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+   <div className=" ">
+
+   </div>
   </div>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,46 @@
 'use client';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { newFeatures } from '../constants';
+
 
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className='flex-[0.75px] flex justify-center flex-col'
+      >
+        <TypingText title="| Recent Project" />
+        <TitleText title={<>What's new about my project?</>} />
+        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
+          {newFeatures.map((feature) => (
+            <NewFeatures
+              key={feature}
+              {...feature}
+            />
+          ))}
+        </div>
+      </motion.div>
+      <motion.div
+        variants={planetVariants('right')}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src='/whats-new.png'
+          alt='get-started'
+          className='w-[90%] h-[90%] object-contain'
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
